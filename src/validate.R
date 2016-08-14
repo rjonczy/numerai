4090e6ae1088e88e5f623eca619c830470b0937b
Robert Jonczy <robert.jonczy@gmail.com>
2016-08-14 12:53:25 +0200

    remove validate helper

--- src/validate.R
+++
@@ -1,9 +0,0 @@
-validate <- function( fit, valid = "validate", model = "" ) {  
-  
-  fcast <- predict(fit, valid)[,2]
-  
-  predOb <- prediction(fcast, valid$target)
-  auc <- performance(predOb, measure = "auc")
-  
-  return(auc)
-}
